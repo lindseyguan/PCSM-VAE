8f7a58757fab1b685084c6f250483075a8dc256e
Lindsey Guan <lguan@mit.edu>
2023-04-24 11:28:21 -0400

    changed decoder to output parameters for normal distribution; implemented initial VAE class

--- models/models.py
+++ models/models.py
@@ -1,3 +1,9 @@
+"""
+Implements VAE for protein condensate + small molecule simulation data.
+
+Based on Pyro's VAE code: https://pyro.ai/examples/vae.html.
+"""
+
 import os
 
 import numpy as np
@@ -24,6 +30,7 @@ class Encoder(nn.Module):
         self.fc_cov = nn.Linear(hidden_dim, z_dim)
 
     def forward(self, x):
+    	# Outputs latent Gaussians
         hidden = self.model(x)
         z_mean = self.fc_mean(hidden) # means
         z_scale = torch.exp(self.fc_cov(hidden)) # square root covariances
@@ -36,45 +43,58 @@ class Decoder(nn.Module):
 	    self.model = nn.Sequential(nn.Linear(z_dim, hidden_dim),
 						           nn.ReLU(),
 						           nn.Linear(hidden_dim, hidden_dim),
-						           nn.ReLU(),
-						           nn.Linear(hidden_dim, output_dim)
-								  )
-        self.sigmoid = nn.Sigmoid()
+						           nn.ReLU()
+        						  )
+
+        self.fc_mean = nn.Linear(hidden_dim, output_dim)
+        self.fc_cov = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, z):
+    	# Outputs Gaussians
         hidden = self.model(z)
-        output = self.sigmoid(hidden) 
-        return output
+        out_mean = self.fc_mean(hidden) # means
+        out_scale = torch.exp(self.fc_cov(hidden)) # square root covariances
+        return out_mean, out_scale
 
 
  class VAE(nn.Module):
-    def __init__(self, z_dim=2, hidden_dim=48, use_cuda=True):
+    def __init__(self, input_dim, z_dim=2, hidden_dim=48, use_cuda=True):
         super().__init__()
-        # create the encoder and decoder networks
-        self.encoder = Encoder(z_dim, hidden_dim)
-        self.decoder = Decoder(z_dim, hidden_dim)
+        self.encoder = Encoder(input_dim=input_dim, 
+        					   z_dim=z_dim, 
+        					   hidden_dim=hidden_dim)
+
+        self.decoder = Decoder(output_dim=input_dim, 
+        					   z_dim=z_dim, 
+        					   hidden_dim=hidden_dim)
 
         if use_cuda:
             self.cuda()
         self.use_cuda = use_cuda
         self.z_dim = z_dim
 
-    # define the model p(x|z)p(z)
+    # define the model, p(x|z)p(z)
     def model(self, x):
-        # register PyTorch module `decoder` with Pyro
+        # register decoder with Pyro
         pyro.module("decoder", self.decoder)
         with pyro.plate("data", x.shape[0]):
             # setup hyperparameters for prior p(z)
             z_loc = x.new_zeros(torch.Size((x.shape[0], self.z_dim)))
             z_scale = x.new_ones(torch.Size((x.shape[0], self.z_dim)))
+
             # sample from prior (value will be sampled by guide when computing the ELBO)
             z = pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
-            # decode the latent code z
-            loc_img = self.decoder(z)
-            # score against actual images
-            pyro.sample("obs", dist.Bernoulli(loc_img).to_event(1), obs=x.reshape(-1, 784))
 
-    # define the guide (i.e. variational distribution) q(z|x)
+            # decode the latent code z
+            decoded_mean, decoded_scale = self.decoder(z)
+
+            # score against actual data
+            pyro.sample("obs", 
+            			dist.Normal(decoded_mean, decoded_scale).to_event(1), 
+            			obs=x.reshape(-1, self.input_dim)
+            		   )
+
+    # define the guide, q(z|x)
     def guide(self, x):
         # register PyTorch module `encoder` with Pyro
         pyro.module("encoder", self.encoder)
@@ -84,13 +104,13 @@ class Decoder(nn.Module):
             # sample the latent code z
             pyro.sample("latent", dist.Normal(z_loc, z_scale).to_event(1))
 
-    # define a helper function for reconstructing images
-    def reconstruct_img(self, x):
-        # encode image x
+    # define a helper function for reconstructing data
+    def reconstruct_data(self, x):
         z_loc, z_scale = self.encoder(x)
+
         # sample in latent space
         z = dist.Normal(z_loc, z_scale).sample()
-        # decode the image (note we don't sample in image space)
-        loc_img = self.decoder(z)
-        return loc_img
+
+        decoded_mean, decoded_scale = self.decoder(z)
+        return decoded_mean, decoded_scale
 
